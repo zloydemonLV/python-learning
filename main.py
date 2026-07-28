fe2d6404207fd32dbdf9d92c5752703bf3604c45
Vladimir <krabowswag@gmail.com>
2026-07-28 22:33:14 +0300

    Learn dictionary methods

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("Hello Github")
+products = [
+    {"name": "Jameson", "price": 25, "stock": 10},
+    {"name": "Jack Daniel's", "price": 30, "stock": 5},
+    {"name": "Glenfiddich", "price": 45, "stock": 2},
+]
+for product in products:
+    print("===Product===")
+    for key, value in product.items():
+        print(f" {key} : {value}")
+    print()
