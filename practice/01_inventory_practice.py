e90647e99b7f867f1a78a325b28d83f1073b4046
Vladimir <krabowswag@gmail.com>
2026-07-31 13:03:27 +0300

    Complete inventory managment practice

--- practice/01_inventory_practice.py
+++ practice/01_inventory_practice.py
@@ -29,6 +29,68 @@ def find_product(products,name):
             return product
 result = find_product(products , "Jameson")
 print(result)
+def add_product(products,name, price, stock):
+    products.append({"name": name, "price": price, "stock": stock})
+add_product(products, "Ballantine's", 28, 9)
+print(products)
+def update_stock(products, name, new_stock):
+    for product in products:
+        if product["name"] == name:
+            product["stock"] = new_stock
+update_stock(products, "Jameson", 20)
+print(products)
+def sell_product(products, name, quantity):
+    for product in products:
+        if product["name"] == name:
+            if product["stock"] >= quantity:
+                product["stock"] -= quantity
+            else:
+                print("Not enough stock")
+sell_product(products, "Jameson", 3)
+print(products)
+def remove_product(products, name):
+    for product in products:
+        if product["name"] == name:
+            products.remove(product)
+remove_product(products, "Jameson")
+print(products)
+def most_expensive_product(products):
+    most_expensive = products[0]
+
+    for product in products:
+            if product["price"] > most_expensive["price"]:
+                most_expensive = product
+    return most_expensive
+result = most_expensive_product(products)
+print(result)
+def cheapest_product(products):
+    cheapest = products[0]
+
+    for product in products:
+        if product["price"] < cheapest["price"]:
+            cheapest = product
+        return cheapest
+
+result = cheapest_product(products)
+print(result)
+def sort_by_price(products):
+    sorted_products = sorted(products, key=lambda product: product["price"])
+    return sorted_products
+
+result = sort_by_price(products)
+print(result)
+def sort_by_stock(products):
+    sorted_products = sorted(products, key=lambda product: product["stock"])
+    return sorted_products
+result = sort_by_stock(products)
+print(result)
+
+
+
+
+
+
+
 
 
 
