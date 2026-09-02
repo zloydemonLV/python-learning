9260a9bbe61f917d2a21eeeaafd2351edc82dbef
Vladimir <krabowswag@gmail.com>
2026-09-02 12:39:14 +0300

    Improve Product stock validation

--- practice/product.py
+++ practice/product.py
@@ -2,12 +2,30 @@ class Product:
     def __init__(self, name, price, stock):
         self.name = name
         self.price = price
-        self.stock = stock
+        self._stock = stock
+    @property
+    def stock(self):
+        return self._stock
+    @stock.setter
+    def stock(self, value):
+        if not isinstance(value, int):
+            raise TypeError("Stock must be an integer")
+
+        if value < 0:
+            raise ValueError("Stock cannot be negative")
+        self._stock = value
+
 
     def show_info(self):
         print(f"Name: {self.name} | Price: {self.price} | Stock: {self.stock}")
 
     def sell(self , quantity):
+        if not isinstance(quantity, int):
+            raise TypeError("Quantity must be an integer")
+
+        if quantity < 0:
+            raise ValueError("Quantity cannot be negative")
+
         if self.stock >= quantity:
             self.stock -= quantity
             print( "Product Sold")
@@ -18,4 +36,5 @@ class Product:
 
     def add_stock(self, quantity):
         self.stock += quantity
+
 
