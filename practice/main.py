475f9e48e4c210422e971e7d724cb28f4a52b7e8
Vladimir <krabowswag@gmail.com>
2026-09-04 12:28:52 +0300

    add prouct creation and update api

--- practice/main.py
+++ practice/main.py
@@ -1,11 +1,17 @@
 from fastapi import FastAPI, HTTPException
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from practice.utils import load_products, save_products
 
 class ProductCreate(BaseModel):
-    name: str
+    name: str = Field(min_length=1, max_length=100)
+    price: int = Field(ge=0)
+    stock: int = Field(ge=0)
+
+class ProductUpdate(BaseModel):
     price: int
     stock: int
+
+
 
 
 
@@ -35,14 +41,34 @@ def get_product(product_name: str):
 def create_product(product: ProductCreate):
     products = load_products()
 
-    new_product = {
+    for existing_product in products:
+        if existing_product["name"].lower == product.name.lower():
+            raise HTTPException(
+                status_code=400,
+                detail="Product name already exists",
+
+             )
+
+        new_product = {
         "name": product.name,
         "price": product.price,
         "stock": product.stock,
     }
-    products.append(new_product)
-    save_products(products)
+        products.append(new_product)
+        save_products(products)
 
-    return new_product
+        return new_product
 
 
+@app.put("/products/{product_name}")
+def update_product(product_name: str, product: ProductUpdate):
+    products = load_products()
+    for existing_product in products:
+        if existing_product["name"].lower() == product_name.lower():
+            existing_product["price"] = product.price
+            existing_product["stock"] = product.stock
+
+            save_products(products)
+            return existing_product
+
+    raise HTTPException(status_code=404, detail="Product not found")
